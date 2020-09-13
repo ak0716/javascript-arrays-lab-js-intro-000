805b8d2b558ae481ddf9095cb0ac4415f218cbb8
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-13 22:12:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
-kittens.push("Ralph");
-return kittens
+destructivelyAppendKitten = () => {
+  kittens.push("Ralph");
+  return kittens
+}
